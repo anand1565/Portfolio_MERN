3cf5a2c1e132499ca4058081e902282d0e6d9235
Anand <ak7610052@gmail.com>
2024-03-21 19:19:20 +0530

    route change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-app.use('/', projectRouter);
+app.use('/api/project', projectRouter);
 app.use('/api/skills/', skillRouter);
 
 app.listen(PORT, () => {
